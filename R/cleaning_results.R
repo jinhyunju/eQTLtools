a2c38beb0e486ef4adb2b99ba83f2041f98d8a1d
Jin-Hyun Ju <jinhyun.ju@gmail.com>
2016-09-17 16:57:08 -0400

    still fixing cleaning_results

--- R/cleaning_results.R
+++ R/cleaning_results.R
@@ -5,10 +5,10 @@ cleaning_results <- function(input.df, snp.info, gene.info, cis_distance){
     #input.df$geno_id <- snp.info[input.df$genotype.idx, "geno_id"]
     input.df <- merge(input.df, snp.info,
                       by.x = "genotype",by.y = "geno_id")
-    input.df <- merge(input.df, gene.info,
+    result.df <- merge(input.df, gene.info,
                       by.x = "phenotype", by.y = "pheno_id")
-    input.df$hit_id <- paste(input.df$genotype,
-                             input.df$phenotype, sep = "_")
+    result.df$hit_id <- paste(result.df$genotype,
+                             result.df$phenotype, sep = "_")
 
     result.df$pheno_symbol <- as.character(result.df$pheno_symbol)
     result.df$pheno_chr <- as.character(result.df$pheno_chr)
